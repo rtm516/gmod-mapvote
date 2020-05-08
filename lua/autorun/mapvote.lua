08edd4d21b69d3e160d0817a81c87d070658bc66
rtm516 <ryantmilner@hotmail.co.uk>
2020-05-08 19:45:01 +0100

    Cleaned up code style and uniformed

--- lua/autorun/mapvote.lua
+++ lua/autorun/mapvote.lua
@@ -14,24 +14,24 @@ MapVoteConfigDefault = {
 }
 --Default Config
 
-hook.Add( "Initialize", "MapVoteConfigSetup", function()
-    if not file.Exists( "mapvote", "DATA") then
-        file.CreateDir( "mapvote" )
+hook.Add("Initialize", "MapVoteConfigSetup", function()
+    if not file.Exists("mapvote", "DATA") then
+        file.CreateDir("mapvote")
     end
-    if not file.Exists( "mapvote/config.txt", "DATA" ) then
-        file.Write( "mapvote/config.txt", util.TableToJSON( MapVoteConfigDefault ) )
+    if not file.Exists("mapvote/config.txt", "DATA") then
+        file.Write("mapvote/config.txt", util.TableToJSON(MapVoteConfigDefault))
     end
-end )
+end)
 
 function MapVote.HasExtraVotePower(ply)
-	-- Example that gives admins more voting power
-	--[[
+    -- Example that gives admins more voting power
+    --[[
     if ply:IsAdmin() then
-		return true
-	end 
+        return true
+    end 
     ]]
 
-	return false
+    return false
 end
 
 
--- lua/mapvote/rtv.lua
+++ lua/mapvote/rtv.lua
@@ -1,11 +1,9 @@
 RTV = RTV or {}
 
 RTV.ChatCommands = {
-	
-	"!rtv",
-	"/rtv",
-	"rtv"
-
+    "!rtv",
+    "/rtv",
+    "rtv"
 }
 
 RTV.TotalVotes = 0
@@ -17,114 +15,102 @@ RTV._ActualWait = CurTime() + RTV.Wait
 RTV.PlayerCount = MapVote.Config.RTVPlayerCount or 3
 
 function RTV.ShouldChange()
-	return RTV.TotalVotes >= math.Round(#player.GetAll()*0.66)
+    return RTV.TotalVotes >= math.Round(#player.GetAll() * 0.66)
 end
 
 function RTV.RemoveVote()
-	RTV.TotalVotes = math.Clamp( RTV.TotalVotes - 1, 0, math.huge )
+    RTV.TotalVotes = math.Clamp(RTV.TotalVotes - 1, 0, math.huge)
 end
 
 function RTV.Start()
-			if GAMEMODE_NAME == "terrortown" then
-				net.Start("RTV_Delay")
-        		net.Broadcast()
- 
-				hook.Add("TTTEndRound", "MapvoteDelayed", function()
-					MapVote.Start(nil, nil, nil, nil)
-				end)
-			elseif GAMEMODE_NAME == "deathrun" then
-				net.Start("RTV_Delay")
-        		net.Broadcast()
+    if GAMEMODE_NAME == "terrortown" then
+        net.Start("RTV_Delay")
+        net.Broadcast()
 
-				hook.Add("RoundEnd", "MapvoteDelayed", function()
-					MapVote.Start(nil, nil, nil, nil)
-				end)
-			else
-				PrintMessage( HUD_PRINTTALK, "The vote has been rocked, map vote imminent")
-				timer.Simple(4, function()
-					MapVote.Start(nil, nil, nil, nil)
-				end)
-			end
+        hook.Add("TTTEndRound", "MapvoteDelayed", function()
+            MapVote.Start(nil, nil, nil, nil)
+        end)
+    elseif GAMEMODE_NAME == "deathrun" then
+        net.Start("RTV_Delay")
+        net.Broadcast()
+
+        hook.Add("RoundEnd", "MapvoteDelayed", function()
+            MapVote.Start(nil, nil, nil, nil)
+        end)
+    else
+        PrintMessage(HUD_PRINTTALK, "The vote has been rocked, map vote imminent")
+        timer.Simple(4, function()
+            MapVote.Start(nil, nil, nil, nil)
+        end)
+    end
 end
 
 
-function RTV.AddVote( ply )
+function RTV.AddVote(ply)
+    if RTV.CanVote(ply) then
+        RTV.TotalVotes = RTV.TotalVotes + 1
+        ply.RTVoted = true
+        MsgN(ply:Nick() .. " has voted to Rock the Vote.")
+        PrintMessage(HUD_PRINTTALK, ply:Nick() .. " has voted to Rock the Vote. (" .. RTV.TotalVotes .. "/" .. math.Round(#player.GetAll() * 0.66) .. ")")
 
-	if RTV.CanVote( ply ) then
-		RTV.TotalVotes = RTV.TotalVotes + 1
-		ply.RTVoted = true
-		MsgN( ply:Nick().." has voted to Rock the Vote." )
-		PrintMessage( HUD_PRINTTALK, ply:Nick().." has voted to Rock the Vote. ("..RTV.TotalVotes.."/"..math.Round(#player.GetAll()*0.66)..")" )
-
-		if RTV.ShouldChange() then
-			RTV.Start()
-		end
-	end
-
+        if RTV.ShouldChange() then
+            RTV.Start()
+        end
+    end
 end
 
-hook.Add( "PlayerDisconnected", "Remove RTV", function( ply )
+hook.Add("PlayerDisconnected", "Remove RTV", function(ply)
+    if ply.RTVoted then
+        RTV.RemoveVote()
+    end
 
-	if ply.RTVoted then
-		RTV.RemoveVote()
-	end
+    timer.Simple(0.1, function()
+        if RTV.ShouldChange() then
+            RTV.Start()
+        end
+    end)
+end)
 
-	timer.Simple( 0.1, function()
+function RTV.CanVote(ply)
+    local plyCount = table.Count(player.GetAll())
 
-		if RTV.ShouldChange() then
-			RTV.Start()
-		end
+    if RTV._ActualWait >= CurTime() then
+        return false, "You must wait a bit before voting!"
+    end
 
-	end )
+    if GetGlobalBool("In_Voting") then
+        return false, "There is currently a vote in progress!"
+    end
 
-end )
+    if ply.RTVoted then
+        return false, "You have already voted to Rock the Vote!"
+    end
 
-function RTV.CanVote( ply )
-	local plyCount = table.Count(player.GetAll())
-	
-	if RTV._ActualWait >= CurTime() then
-		return false, "You must wait a bit before voting!"
-	end
-
-	if GetGlobalBool( "In_Voting" ) then
-		return false, "There is currently a vote in progress!"
-	end
-
-	if ply.RTVoted then
-		return false, "You have already voted to Rock the Vote!"
-	end
-
-	if RTV.ChangingMaps then
-		return false, "There has already been a vote, the map is going to change!"
-	end
-	if plyCount < RTV.PlayerCount then
+    if RTV.ChangingMaps then
+        return false, "There has already been a vote, the map is going to change!"
+    end
+    if plyCount < RTV.PlayerCount then
         return false, "You need more players before you can rock the vote!"
     end
 
-	return true
-
+    return true
 end
 
-function RTV.StartVote( ply )
+function RTV.StartVote(ply)
+    local can, err = RTV.CanVote(ply)
 
-	local can, err = RTV.CanVote(ply)
+    if not can then
+        ply:PrintMessage(HUD_PRINTTALK, err)
+        return
+    end
 
-	if not can then
-		ply:PrintMessage( HUD_PRINTTALK, err )
-		return
-	end
-
-	RTV.AddVote( ply )
-
+    RTV.AddVote(ply)
 end
+concommand.Add("rtv_start", RTV.StartVote)
 
-concommand.Add( "rtv_start", RTV.StartVote )
-
-hook.Add( "PlayerSay", "RTV Chat Commands", function( ply, text )
-
-	if table.HasValue( RTV.ChatCommands, string.lower(text) ) then
-		RTV.StartVote( ply )
-		return ""
-	end
-
-end )
+hook.Add("PlayerSay", "RTV Chat Commands", function(ply, text)
+    if table.HasValue(RTV.ChatCommands, string.lower(text)) then
+        RTV.StartVote(ply)
+        return ""
+    end
+end)
